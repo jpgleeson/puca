551d2453d573ec91c92d4f5f52cda366471ae6c7
jack gleeson <jack@jpgleeson.com>
2023-12-06 19:31:14 -0800

    Parse an obj. Annoyingly raylib bindings aren't exposing DrawTriangle3D

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	camera.Fovy = 45.0
 	camera.Projection = rl.CameraPerspective
 
-	loadObjText()
+	modelTriangles := loadObjText()
 
 	defer rl.CloseWindow()
 
@@ -30,14 +30,27 @@ func main() {
 		rl.ClearBackground(rl.LightGray)
 		rl.BeginMode3D(camera)
 		rl.DrawGrid(20, 10.0)
+		for _, face := range modelTriangles {
+			rl.DrawPoint3D(face.Point1, rl.Blue)
+			rl.DrawPoint3D(face.Point2, rl.Blue)
+			rl.DrawPoint3D(face.Point3, rl.Blue)
+		}
 		rl.EndMode3D()
 		rl.EndDrawing()
 	}
 }
 
-func loadObjText() {
+type modelFace struct {
+	Point1 rl.Vector3
+	Point2 rl.Vector3
+	Point3 rl.Vector3
+}
+
+func loadObjText() []modelFace {
 	vertices := make(map[int]rl.Vector3)
 	verticeIndex := 1
+
+	faces := make([]modelFace, 0)
 
 	file, err := os.Open("Spearman.obj")
 	if err != nil {
@@ -50,16 +63,43 @@ func loadObjText() {
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
 		lineComponents := strings.Split(fileScanner.Text(), " ")
-		fmt.Printf("line: %s\n", line)
+		// fmt.Printf("line: %s\n", line)
 		lineType := lineComponents[0]
+		switch lineType {
 		// Lines that start with v are vertex declarations
-		if lineType == "v" {
-			fmt.Printf("This line is a vertex")
+		case "v":
 			vertexX := stringToFloat32(lineComponents[1])
 			vertexY := stringToFloat32(lineComponents[2])
 			vertexZ := stringToFloat32(lineComponents[3])
 			vertices[verticeIndex] = rl.NewVector3(vertexX, vertexY, vertexZ)
 			verticeIndex = verticeIndex + 1
+		case "vn":
+			// fmt.Println("This line is a normal")
+		case "f":
+			point1Components := strings.Split(lineComponents[1], "/")
+			point2Components := strings.Split(lineComponents[2], "/")
+			point3Components := strings.Split(lineComponents[3], "/")
+
+			point1Vertex, err := strconv.Atoi(point1Components[0])
+			if err != nil {
+				fmt.Println("Error converting to int")
+			}
+			point2Vertex, err := strconv.Atoi(point2Components[0])
+			if err != nil {
+				fmt.Println("Error converting to int")
+			}
+			point3Vertex, err := strconv.Atoi(point3Components[0])
+			if err != nil {
+				fmt.Println("Error converting to int")
+			}
+
+			faces = append(faces, modelFace{
+				Point1: vertices[point1Vertex],
+				Point2: vertices[point2Vertex],
+				Point3: vertices[point3Vertex],
+			})
+		default:
+			fmt.Sprintln("Unknown line type %s", line)
 		}
 	}
 
@@ -67,8 +107,7 @@ func loadObjText() {
 		log.Fatal(err)
 	}
 
-	fmt.Print(vertices)
-
+	return faces
 }
 
 func stringToFloat32(input string) float32 {
