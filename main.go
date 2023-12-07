f3ff50799c64e407d0475b6a3594fc7d0bf8d0da
jack gleeson <jack@jpgleeson.com>
2023-12-06 18:17:30 -0800

    Get a camera

--- main.go
+++ main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
-	"fmt"
-
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 func main() {
-	fmt.Println("Test")
 	rl.InitWindow(800, 600, "puca")
+
+	camera := rl.Camera{}
+	camera.Position = rl.NewVector3(10.0, 10.0, 10.0)
+	camera.Target = rl.NewVector3(0, 0, 0)
+	camera.Up = rl.NewVector3(0, 1, 0)
+	camera.Projection = rl.CameraPerspective
+
 	defer rl.CloseWindow()
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.LightGray)
+		rl.BeginMode3D(camera)
+		rl.EndMode3D()
 		rl.EndDrawing()
 	}
 }
