0ebf78a9dd7b9dfac379f4d472ed65655d18a9c9
jack gleeson <jack@jpgleeson.com>
2023-12-06 18:53:16 -0800

    Start parsing text objs

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
@@ -8,10 +15,13 @@ func main() {
 	rl.InitWindow(800, 600, "puca")
 
 	camera := rl.Camera{}
-	camera.Position = rl.NewVector3(10.0, 10.0, 10.0)
-	camera.Target = rl.NewVector3(0, 0, 0)
-	camera.Up = rl.NewVector3(0, 1, 0)
+	camera.Position = rl.NewVector3(100.0, 10.0, 10.0)
+	camera.Target = rl.NewVector3(0.0, 10.0, 0.0)
+	camera.Up = rl.NewVector3(0.0, 1.0, 0.0)
+	camera.Fovy = 45.0
 	camera.Projection = rl.CameraPerspective
+
+	loadObjText()
 
 	defer rl.CloseWindow()
 
@@ -19,7 +29,53 @@ func main() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.LightGray)
 		rl.BeginMode3D(camera)
+		rl.DrawGrid(20, 10.0)
 		rl.EndMode3D()
 		rl.EndDrawing()
 	}
+}
+
+func loadObjText() {
+	vertices := make(map[int]rl.Vector3)
+	verticeIndex := 1
+
+	file, err := os.Open("Spearman.obj")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	fileScanner := bufio.NewScanner(file)
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		lineComponents := strings.Split(fileScanner.Text(), " ")
+		fmt.Printf("line: %s\n", line)
+		lineType := lineComponents[0]
+		// Lines that start with v are vertex declarations
+		if lineType == "v" {
+			fmt.Printf("This line is a vertex")
+			vertexX := stringToFloat32(lineComponents[1])
+			vertexY := stringToFloat32(lineComponents[2])
+			vertexZ := stringToFloat32(lineComponents[3])
+			vertices[verticeIndex] = rl.NewVector3(vertexX, vertexY, vertexZ)
+			verticeIndex = verticeIndex + 1
+		}
+	}
+
+	if err := fileScanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(vertices)
+
+}
+
+func stringToFloat32(input string) float32 {
+	parsedValue, err := strconv.ParseFloat(input, 32)
+	if err != nil {
+		fmt.Printf("Error parsing X")
+	}
+	floatValue := float32(parsedValue)
+	return floatValue
 }
